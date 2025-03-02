104dc6420cc5665972bd100807fab5681ff1a73d
Shubham Jha <shubhamjha202@gmail.com>
2025-03-02 21:51:21 +0530

    added google stun server

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -4,6 +4,12 @@ const _DEFAULT_CONFIG = {
   'iceServers': [
     {'urls': "stun:stun.bethesda.net:3478"},
     {
+      'urls': [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+      ]
+    },
+    {
       "urls": [
         "turn:eu-0.turn.peerjs.com:3478",
         "turn:us-0.turn.peerjs.com:3478",
